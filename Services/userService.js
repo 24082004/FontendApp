325303bd2d45170e677d19ec34d4c5141b4759cc
Nguyen Canh <canhn2000@gmail.com>
2025-06-27 18:32:25 +0700

    Complete call api ProfileScreen

---
+++ Services/userService.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://my-backend-api-movie.onrender.com/api'; // thay bằng URL thực tế
+
+export const getProfile = async (token) => {
+  try {
+    const res = await axios.get(`${API_BASE_URL}/users/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return res.data;
+  } catch (err) {
+    console.error('Lỗi lấy profile:', err);
+    throw err;
+  }
+};
